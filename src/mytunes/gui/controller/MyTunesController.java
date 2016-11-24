8fb1df810abd6e3f42a78bc8d86f0288eab58166
Adam Hansen <adamlars90@gmail.com>
2016-11-24 11:27:45 +0100

    Added structure and initial design

--- src/mytunes/gui/controller/MyTunesController.java
+++ src/mytunes/gui/controller/MyTunesController.java
@@ -7,10 +7,16 @@ package mytunes.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import mytunes.be.Playlist;
+import mytunes.be.Song;
 
 /**
  *
@@ -19,13 +25,41 @@ import javafx.scene.control.Label;
 public class MyTunesController implements Initializable {
 
     @FXML
-    private Label label;
-
+    private TableView<Playlist> tablePlaylists;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private ListView<Song> listPlaylist;
+    @FXML
+    private TableView<Song> tableSongs;
+    @FXML
+    private Label lblIsPlaying;
+    @FXML
+    private TextField txtSearch;
+    @FXML
+    private Button btnPlay;
+    @FXML
+    private Button btnPrevious;
+    @FXML
+    private Button btnNext;
+    @FXML
+    private Slider sliderVolume;
+    @FXML
+    private Button btnDeletePlaylist;
+    @FXML
+    private Button btnSearch;
+    @FXML
+    private Button btnMoveSongUp;
+    @FXML
+    private Button btnMoveSongDown;
+    @FXML
+    private Button btnRemoveFromList;
+    @FXML
+    private Button btnAddSong;
+    @FXML
+    private Button btnEditSong;
+    @FXML
+    private Button btnDeleteSong;
+    @FXML
+    private Button btnAddToList;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
