a81bbd6009dae374b7789cc4e0c6f7df5f8f7cd6
Sk0vgaard <Sk0vgaard@live.dk>
2016-11-28 11:07:33 +0100

    Assigned Ids for buttons in NewEditView.

--- src/mytunes/gui/controller/NewEditSongController.java
+++ src/mytunes/gui/controller/NewEditSongController.java
@@ -7,7 +7,12 @@ package mytunes.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import mytunes.gui.model.SongModel;
 
 /**
@@ -18,6 +23,22 @@ import mytunes.gui.model.SongModel;
 public class NewEditSongController implements Initializable
 {
     private SongModel songModel;
+    @FXML
+    private TextField txtTitle;
+    @FXML
+    private TextField txtArtist;
+    @FXML
+    private ComboBox<?> comboGenre;
+    @FXML
+    private Button btnMore;
+    @FXML
+    private TextField txtDuration;
+    @FXML
+    private TextField txtFile;
+    @FXML
+    private Button btnChoose;
+    @FXML
+    private Button btnSave;
 
     /**
      * Initializes the controller class.
@@ -28,5 +49,20 @@ public class NewEditSongController implements Initializable
         // TODO
         songModel = SongModel.getInstance();
     }    
+
+    @FXML
+    private void handleMoreButton(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void handleChooseButton(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void handleSaveButton(ActionEvent event)
+    {
+    }
     
 }
