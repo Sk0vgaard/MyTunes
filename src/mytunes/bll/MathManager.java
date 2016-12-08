a17d7bd604259747d5728030fb2543b76db8d442
LindvedKrvang <rklindved@gmail.com>
2016-12-08 10:12:44 +0100

    Added comments to the MathManager

--- src/mytunes/bll/MathManager.java
+++ src/mytunes/bll/MathManager.java
@@ -5,7 +5,6 @@
  */
 package mytunes.bll;
 
-import com.sun.deploy.ui.UIFactory;
 import java.io.Serializable;
 import java.util.ArrayList;
 import mytunes.be.Song;
@@ -53,9 +52,10 @@ public class MathManager implements Serializable {
         int hours = 0;
         int minutes = 0;
         int seconds = 0;
+        //Goes through all songs and add their duration.
         for (Song song : list) {
             String[] placeHolderString = song.getDuration().get().split(":");
-            if(placeHolderString.length >2)
+            if(placeHolderString.length > 2)
             {
                 hours += Integer.parseInt(placeHolderString[0]);
                 minutes += Integer.parseInt(placeHolderString[1]);
@@ -67,34 +67,34 @@ public class MathManager implements Serializable {
                 seconds += Integer.parseInt(placeHolderString[1]);
             }
         }
-
-        if (seconds >= A_MINUTE) {
+        //If seconds is above 60, divide by 60 and add that to minutes.
+        if (seconds >= A_MINUTE) { 
             minutes += seconds / A_MINUTE;
             seconds = seconds % A_MINUTE;
         }
-                
+        //Combine minutes and seconds and then convert it to actual minutes and seconds.        
         double totalDuration = (double) minutes + (double) seconds / (double) A_MINUTE;
         totalDuration = convertToMinutes(totalDuration);
-        
+        //Checks if there is more than 60 minutes and add that to hours.
         if(totalDuration > A_MINUTE)
         {
             double placeholder = totalDuration / A_MINUTE;            
             hours += (int) placeholder;
             totalDuration = totalDuration % A_MINUTE;
         }
-        
+        //Make a String that holds minutes and seconds and only allowed to show to decimals.
         String stringDuration = String.format("%.2f", totalDuration);
-        
+        //Checks if minutes is less than 10 and add a zero infront of it for good looking.
         if(totalDuration < 10)
         {
             stringDuration = 0 + stringDuration;
         }
-        
+        //Checks if duration is over an hour. If so, add the hours to the duration.
         if(hours > 0)
         {            
             stringDuration = hours + "," + stringDuration;
         }
-        
+        //Replace all "," with ":" for good looking.
         stringDuration = stringDuration.replace(",", ":");
         
         return stringDuration;
